dff2f415765995483340f2b4bbc69df390fcd733
Koro_140 <ahmedhussien873@gmail.com>
2026-07-17 04:33:40 +0300

    Added windows support

--- include/player.h
+++ include/player.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <pthread.h>
 #include <stdbool.h>
-#include <stdatomic.h>
 
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
@@ -11,6 +9,40 @@
 #include <libswresample/swresample.h>
 
 #include "frame_queue.h"
+
+
+#ifdef _WIN32
+typedef struct Player {
+	AVFormatContext* format_context;
+	AVPacket* packet;
+
+	const AVCodec* video_codec;
+	AVCodecContext* video_decoder;
+	AVFrame* frame;
+	AVStream* video_stream;
+	enum AVPixelFormat last_fmt;
+
+	const AVCodec* audio_codec;
+	AVCodecContext* audio_decoder;
+	AVFrame* audio_frame;
+	AVFrame* resampled_frame;
+	struct SwrContext* swr;
+
+	int video_stream_idx;
+	int audio_stream_idx;
+
+	double video_timebase;
+	double audio_timebase;
+
+	Frame_Queue* video_queue_referenece;
+	Frame_Queue* audio_queue_referenece;
+
+	volatile LONG quit;
+	HANDLE thread;
+}Player;
+
+
+#else
 
 typedef struct Player {
   AVFormatContext *format_context;
@@ -40,6 +72,8 @@ typedef struct Player {
   atomic_bool quit;
   pthread_t   thread;
 }Player;
+
+#endif // _WIN32
 
 bool player_init(Player *player, Frame_Queue* video_queue, Frame_Queue* audio_queue, const char *url);
 void player_destroy(Player *player);
